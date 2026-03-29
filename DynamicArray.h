ef487f6b705358e540bb5f49472422f957d1b583
amrosafory <amrosafory@aucegypt.edu>
2026-03-29 22:04:22 +0200

    Added comments and changed List and Stack to match uml

---
+++ DynamicArray.h
@@ -0,0 +1,112 @@
+#ifndef DYNAMIC_ARRAY_H
+#define DYNAMIC_ARRAY_H
+
+#include <stdexcept>
+
+template <typename T>
+class DynamicArray {
+private:
+    T* data;
+    int capacity;
+    int size;
+
+    // doubles the capacity
+    void resize();
+
+public:
+    DynamicArray(); // default constructor
+    ~DynamicArray(); // destructor
+
+    void add(T item); // appends item to the end of the array
+    T get(int index); // returns the element at index
+    T& operator[](int index); // returns a reference to the element at index
+    void set(int index, T item); // replaces the element at index with item
+    void removeAt(int index); // removes the element at index
+    int getSize(); // returns the number of elements
+    bool isEmpty(); // returns true if the array is empty
+};
+
+template <typename T>
+DynamicArray<T>::DynamicArray() {
+    capacity = 4;
+    size = 0;
+    data = new T[capacity];
+}
+
+template <typename T>
+DynamicArray<T>::~DynamicArray() {
+    delete[] data;
+}
+
+template <typename T>
+void DynamicArray<T>::resize() {
+    capacity *= 2;
+    T* newData = new T[capacity];
+
+    for (int i = 0; i < size; i++)
+        newData[i] = data[i];
+
+    delete[] data;
+    data = newData;
+}
+
+template <typename T>
+void DynamicArray<T>::add(T item) {
+    // resize if full
+    if (size == capacity)
+        resize();
+
+    data[size] = item;
+    
+    // update size
+    size++;
+}
+
+template <typename T>
+T DynamicArray<T>::get(int index) {
+    if (index < 0 || index > size - 1)
+        throw std::out_of_range("Index out of range");
+
+    return data[index];
+}
+
+template <typename T>
+T& DynamicArray<T>::operator[](int index) {
+    if (index < 0 || index >= size)
+        throw std::out_of_range("Index out of range");
+
+    return data[index];
+}
+
+template <typename T>
+void DynamicArray<T>::set(int index, T item) {
+    if (index < 0 || index > size - 1)
+        throw std::out_of_range("Index out of range");
+
+    data[index] = item;
+}
+
+template <typename T>
+void DynamicArray<T>::removeAt(int index) {
+    if (index < 0 || index > size - 1)
+        throw std::out_of_range("Index out of range");
+
+    // shift left
+    for (int i = index; i < size - 1; i++)
+        data[i] = data[i + 1];
+
+    // update size
+    size--;
+}
+
+template <typename T>
+int DynamicArray<T>::getSize() {
+    return size;
+}
+
+template <typename T>
+bool DynamicArray<T>::isEmpty() {
+    return size == 0;
+}
+
+#endif
---
+++ Stack.h
@@ -0,0 +1,87 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdexcept>
+
+template <typename T>
+class Stack {
+private:
+    T* elements;
+    int top;
+    int capacity;
+
+    // doubles the capacity
+    void resize();
+
+public:
+    Stack(); // default constructor
+    ~Stack(); // destructor
+
+    void push(T item); // pushes item onto the stack
+    T pop(); // removes the top item and returns it
+    T peek(); // returns the top item
+    bool isEmpty(); // if stack is empty, the record is valid
+    int getSize(); // return the number of elements
+};
+
+template <typename T>
+Stack<T>::Stack() {
+    // default capacity
+    capacity = 4;
+    top = -1;
+    elements = new T[capacity];
+}
+
+template <typename T>
+Stack<T>::~Stack() {
+    delete[] elements;
+}
+
+template <typename T>
+void Stack<T>::resize() {
+    capacity *= 2;
+    T* newElements = new T[capacity];
+
+    for (int i = 0; i < top + 1; i++)
+        newElements[i] = elements[i];
+
+    delete[] elements;
+    elements = newElements;
+}
+
+template <typename T>
+void Stack<T>::push(T item) {
+    // resize if full
+    if (top + 1 == capacity)
+        resize();
+
+    elements[++top] = item;
+}
+
+template <typename T>
+T Stack<T>::pop() {
+    if (isEmpty())
+        throw std::runtime_error("Stack is empty");
+
+    return elements[top--];
+}
+
+template <typename T>
+T Stack<T>::peek() {
+    if (isEmpty())
+        throw std::runtime_error("Stack is empty");
+
+    return elements[top];
+}
+
+template <typename T>
+bool Stack<T>::isEmpty() {
+    return top == -1;
+}
+
+template <typename T>
+int Stack<T>::getSize() {
+    return top + 1;
+}
+
+#endif
